5b937d00947000190f1a4dd2f6e243059ca2a46f
Ben Scholer <benscholer3248511@gmail.com>
2018-04-23 18:22:31 -0400

    Fixing minute issue

--- Church/index.js
+++ Church/index.js
@@ -29,6 +29,11 @@ function calculate() {
         else {
             stopOutput = stop[1] + " - " + hours + ":" + (minutes - stop[0]) + "<br/>";
         }
+        console.log(stopOutput);
+        // Check to see if we need a 0 in front of the minutes
+        if (stopOutput.substring(stopOutput.indexOf(":")).length === 1) {
+            stopOutput = stopOutput.substring(0, stopOutput.indexOf(":")) + "0" + stopOutput.substring(stopOutput.indexOf(":"));
+        }
         newStops += stopOutput;
     }
     console.log(newStops);
